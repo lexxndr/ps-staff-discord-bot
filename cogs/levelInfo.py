24e7b1e28565b9fc474d1c505d020e7915852426
lexxndr <91974210+lexxndr@users.noreply.github.com>
2025-05-15 23:52:40 +0300

    updated (check desc for the pr)
    level info thing and level list properly formatted

--- cogs/levelInfo.py
+++ cogs/levelInfo.py
@@ -4,14 +4,13 @@ from typing import Sequence
 
 env = dotenv.dotenv_values('.env') or os.environ
 
-#levelList = json.loads(open('./json/newlevel.json', 'r').read())
-
 class levelInfo(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.levelList = {}
-        self.emojiList = {"Overview": "<:Overview:1372561106219827341>", "Description": "<:Description:1372561094014537830>", "Objective": "<:Objective:1372561114524553226>"}
-    
+        self.emojiList = {"Badges": "<:Overview:1372561106219827341>", "Description": "<:Description:1372561094014537830>", "Tutorial": "<:Objective:1372561114524553226>", "Entities": "<:Skull:1372670255284883476>", "Fun Facts": "<:Tips:1372670198125035571>", "Level Type": "<:Note:1372670234841845973>"}
+            # available emojis
+            # <:Origin:1372670213702811728>; <:Question:1372670182937460766>; <:Exit:1372670158610632905>; <:Gallery:1372670132249296946>; <:Wrench:1372670271957368893>
     @commands.Cog.listener()
     async def on_ready(self):
         async with aiohttp.ClientSession() as session:
@@ -38,27 +37,33 @@ class levelInfo(commands.Cog):
     async def levelinfo(self, interaction: disnake.CommandInteraction, level: str): # I understand it now.
         content = self.levelList[level]
         sections = content['sections']
-        image = content['image']
+        image1 = content['image1']
+        image2 = content['image2']
 
         components: Sequence[ui.UIComponent] = [ # init components v2
             ui.Container(
-                *[
+                ui.TextDisplay(f'# {level}'),
+                *[ # i have no idea how this works but its perfect and i dont want to touch this EVER
                     item
                     for key, value in sections.items()
                     for item in (
-                        ui.TextDisplay(f"## {self.emojiList.get(key, '')} {key}\n{value}"),
-                        ui.Separator(spacing=disnake.SeparatorSpacingSize.large),
+                        ui.TextDisplay(f"## {self.emojiList.get(key, '')}  {key}\n**    {value}**"),
+                        ui.Separator(spacing=disnake.SeparatorSpacingSize.small),
                     )
                 ],
-                ui.MediaGallery(
+                ui.MediaGallery( # add more mediagalleryitem objects if need more images but it may break everything
                     disnake.MediaGalleryItem(
                         {
-                            "media": {"url": image},
-                            "description": "The image of the specified level.",
+                            "media": {"url": image1},
+                        },
+                    ),
+                    disnake.MediaGalleryItem(
+                        {
+                            "media": {"url": image2},
                         },
                     )
-                ),
-                accent_colour=disnake.Colour(0xEE99CC),
+                ), # for now only 2 screenshots are being used so dont change this
+                accent_colour=disnake.Colour(0xf0b000),
                 spoiler=False,
             ),
         ]
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-import disnake, dotenv, os
+import disnake, dotenv
 from disnake.ext import commands
 
 env = dotenv.dotenv_values('.env')
 
 intents = disnake.Intents.default()
 intents.message_content = True
-bot = commands.Bot(intents=intents,test_guilds=[1226166880960774225,1372528511985782877])
+bot = commands.InteractionBot(intents=intents,test_guilds=[1226166880960774225,1372528511985782877])
 
 @bot.event
 async def on_ready():
